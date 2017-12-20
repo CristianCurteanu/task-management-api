cd1a10c5b1d500c8c076849941e3951b8719ad69
Cristian Curteanu <Cristian.Curteanu@4finance.com>
2017-12-20 10:16:58 +0200

    [WIP] Migrated Client model to MongoDB

---
+++ tests/session_token_model_test.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	. "models/session_token"
+	"time"
+
+	. "gopkg.in/check.v1"
+)
+
+var model = new(SessionToken)
+
+func (s *ClientSuite) TestSessionTokenCreation(check *C) {
+	new(SessionToken).Initialize().RemoveAll(nil)
+
+	tokenCollection := new(SessionToken).Initialize()
+	sessionToken := SessionToken{Token: "some-cr3epY-t0keN", ExpirestAt: time.Now()}
+
+	insertError := tokenCollection.Insert(sessionToken)
+	check.Assert(insertError, IsNil)
+}
